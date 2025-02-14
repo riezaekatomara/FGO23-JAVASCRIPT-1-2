b23d4d48962bf7b7990bf6c91af3943ec79608e2
riezaekatomara <130181217+riezaekatomara@users.noreply.github.com>
2025-02-14 15:26:08 +0700

    Mini Task Day 4 Week 1 FGO23 SOAL 1

---
+++ soal1-day4.js
@@ -0,0 +1,13 @@
+const movie = {
+    id: 1,
+    title: "Avenger",
+    image: "Avenger.jpg",
+    genre: ["Action", "Superhero", "Adventure"],
+    cast: [
+        { name: "Steve Rodgers", role: "Captain America" },
+        { name: "Tony Stark", role: "Iron Man" },
+        { name: "Bruce Banner", role: "Hulk" },
+        { name: "Chris Hemsworth", role: "Thor" },
+        { name: "Natasha Romanoff", role: "Black Widow" }
+    ],
+}
